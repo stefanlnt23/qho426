602c0ebe9203c343bfa13731a96a15d249dafce7
stefanlenta <>
2021-07-16 01:57:03 +0000

    Printing characters with print command

---
+++ Basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+print ("\n"" hello" "\n""\t""\t""hows going")

00e980b6b3476a68915b965c58057adc0cbcc6fd
stefanlenta <>
2021-07-16 04:31:42 +0000

    Added code to demonstrate the use of string operations

---
+++ Basics/input/string_operators.py
@@ -0,0 +1,21 @@
+number_lives = int(input(" Please enter the number of lives: "))
+print ("\n")
+energy_level = int(input(" Please enter the number of energy: "))
+print ("\n")
+shield_level = int(input(" Please enter the shield level: "))
+print ("\n")
+print (" Health have been set!")
+print ("\n")
+print ("\n")
+
+
+lives = "♥"
+energy = "♦"
+shield =  "♦"
+
+print ("Lives:"+lives*number_lives)
+print ("\n")
+print ("Energy:"+energy*energy_level)
+print ("\n")
+print("Shield:"+shield*shield_level)
+print ("\n")

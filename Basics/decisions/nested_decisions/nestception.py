ca12e620e7298980fbff6b3e9575201420b45c77
stefanlenta <>
2021-07-17 11:00:40 +0000

    Added code to demonstrate the use of multiple statement

---
+++ Basics/decisions/nested_decisions/nestception.py
@@ -0,0 +1,46 @@
+print (" Where should i look boss man?")
+look = input ()
+
+
+
+
+if (look == "in the bedroom"):
+  print ("where in the bedroom should i look boss man?")
+  where = input()
+  if (where == "under the bed"):
+    print (" Found some shoes but no battery!")
+  else:
+    print (" Found some mess but no battery boss man!")
+
+
+
+
+
+
+if (look == "in the bathroom"):
+  print (" Where in the bathroom should i look boss man?")
+  where = input()
+  if (where == "in the bathtub"):
+    print (" Found a rubber duck but no battery!")
+  else:
+    print (" Found a wet surface but no batteryyyyyyyyyy!")
+
+
+
+
+
+
+if ( look == "in the lab"):
+  print (" Where in the lab should i look?")
+  where =  input()
+  if (where == "on the table"):
+    print (" Finaly boss man! it was time..")
+  else:
+    print (" You are useless boss man! ")
+
+
+
+
+
+else:
+  print (" I dont know where that is but i will keep looking boss man...")

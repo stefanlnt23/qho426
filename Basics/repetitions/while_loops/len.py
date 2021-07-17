2ee5b33da951dce80c26bc6aeb2770407ec6b328
stefanlenta <>
2021-07-17 17:56:00 +0000

    Function with len

---
+++ Basics/repetitions/while_loops/len.py
@@ -0,0 +1,8 @@
+#prints instruction to the user
+print ("Please enter a phrase : ")
+#user input the word
+read = input(":")
+# function that is attached to the user input
+len(read)
+#prints 
+print ( "boop "*len(read))
